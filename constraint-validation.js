c18e9623e456af71e44388cf8f2a7e2bc913b4dc
Boaz-Dejene-Et <boazdeju@gmail.com>
2022-12-06 14:45:29 +0300

    update

--- constraint-validation.js
+++ constraint-validation.js
@@ -1,7 +1,44 @@
 // TODO
-const regex = '/\w+@\w+\.\w+/'
+document.addEventListener("DOMContentLoaded", function () {
 
-const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("first")
-}
+    const firstName = document.getElementById('first-name');
+    const lastName = document.getElementById('last-name');
+    const email = document.getElementById('email');
+    const form = document.getElementById('connect-form');
+
+    function checkValidation(e) {
+        let currentElement = e.target;
+        // console.log(`id:${currentElement.id}, value:${currentElement.value}, length:${currentElement.value.length}
+        // , valid:${currentElement.validity.valid}`);
+        
+        // without this the tool tip keeps showing!
+        currentElement.setCustomValidity("");
+        if (currentElement.id === 'first-name' ||
+            currentElement.id === 'last-name') {
+            if (!currentElement.validity.valid) {
+                currentElement.setCustomValidity(`Name has to be minimum of ${currentElement.minLength} letters`);
+                currentElement.reportValidity();
+            }
+        } else if (currentElement.id === 'email') {
+            if (!currentElement.validity.valid) {
+                currentElement.setCustomValidity(`Has to be a valid Email`);
+                currentElement.reportValidity();
+            }
+        }
+    }
+    firstName.addEventListener('input', checkValidation);
+    lastName.addEventListener('input', checkValidation);
+    email.addEventListener('input', checkValidation)
+
+    form.addEventListener('submit', (e) => {
+        const validationElements = Array.from(document.querySelectorAll('.validate-input'));
+        validationElements.forEach(el => {
+
+            if (!el.validity.valid) {
+                el.reportValidity();
+                e.preventDefault();
+            }
+
+        });
+    });
+});
